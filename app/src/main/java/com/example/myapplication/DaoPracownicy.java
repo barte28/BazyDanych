ffaf90450d2436b4a1e3d642f037a0ab549a790e
barte28 <bartek.teuerle@gmail.com>
2025-04-02 11:10:28 +0200

    start

---
+++ app/src/main/java/com/example/myapplication/DaoPracownicy.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface DaoPracownicy {
+
+    @Insert
+    public void dodajPracownika(Pracownik pracownik);
+
+    @Insert
+    public void dodajWieluPracownikow(Pracownik ... pracownicy);
+
+    @Delete
+    public void usunPracownika(Pracownik pracownik);
+
+    @Update
+    public void zakutalizujDanePracownika(Pracownik pracownik);
+
+    @Query("Select * from pracownicy where jezykOjczysty = 'polski'")
+    public List<Pracownik> wypiszPracownikowPolskoJezycznych();
+
+    @Query("Select * from pracownicy where jezykObcy = :jezyk")
+    public List<Pracownik> wypiszPracownikowmowiacychjezykiem(String jezyk);
+
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DataBasePracownicy dataBasePracownicy;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RoomDatabase.Callback mojCallback = new RoomDatabase.Callback() {
+            @Override
+            public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                super.onCreate(db);
+            }
+
+            @Override
+            public void onOpen(@NonNull SupportSQLiteDatabase db) {
+                super.onOpen(db);
+            }
+        };
+        dataBasePracownicy = Room.databaseBuilder(
+                getApplicationContext(),
+                DataBasePracownicy.class,
+                "PracownicyDB").addCallback(mojCallback).
+                allowMainThreadQueries().build();
+
+        dodajDaneDoBazy();
+    }
+
+    private void dodajDaneDoBazy(){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        executorService.execute(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        dataBasePracownicy.getDaoPracownicy().dodajPracownika(
+                                new Pracownik(
+                                        "Jaś",
+                                        "Nowak",
+                                        "polski",
+                                        "angielski",
+                                        12300.99,
+                                        "programista"));
+                        handler.post(
+                                new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(MainActivity.this, "Dodano do bazy", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                        );
+                    }
+                }
+        );
+    }
+
+}
